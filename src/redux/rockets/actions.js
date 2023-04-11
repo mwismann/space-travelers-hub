31e02e9ec421eea27d895399526bba307531416b
Matt <mwbertolucci@gmail.com>
2023-04-11 16:21:53 -0400

    Add action to fetch rockets data from API

---
+++ src/redux/rockets/actions.js
@@ -0,0 +1,18 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const URL = 'https://api.spacexdata.com/v3/rockets';
+
+const getRockets = createAsyncThunk(
+  'rockets/getRockets',
+  async () => {
+    try {
+      const { data } = await axios.get(URL);
+      return data;
+    } catch (error) {
+      return error;
+    }
+  }
+);
+
+export default getRockets;
