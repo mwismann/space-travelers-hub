1b9e7317b7dcf09c38d58ebd8e179c9a06c87144
getssh <gtamrat33@gmail.com>
2023-04-12 01:44:44 +0300

    Add try catch block to avoid unexpected failure

--- src/redux/missions/missionSlice.js
+++ src/redux/missions/missionSlice.js
@@ -3,9 +3,13 @@ import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 const url = 'https://api.spacexdata.com/v3/missions';
 
 export const getMissons = createAsyncThunk('missions/getMission',
-  async(_, {rejectWithValue}) => {
-    const response = await fetch(url)
-    const data = response.json()
-    return data
+async(_, {rejectWithValue}) => {
+  try {
+      const response = await fetch(url)
+      const data = response.json()
+      return data
+    } catch (error) {
+      return rejectWithValue({message: error.message})
+    }
   }
 )
