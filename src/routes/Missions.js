ccf5172b71c3fd7fc80cf1fce2edffc2becc1d8f
getssh <gtamrat33@gmail.com>
2023-04-12 12:15:14 +0300

    Render mission values from store by using useSelector and useDispatch

--- src/routes/Missions.js
+++ src/routes/Missions.js
@@ -1,5 +1,18 @@
-const Missions = () => (
-  <div>Missions</div>
-);
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMissons } from '../redux/missions/missionSlice';
+
+const Missions = () => {
+  const { missions } = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMissons());
+  }, [dispatch]);
+
+  return (
+    <div>{JSON.stringify(missions)}</div>
+  );
+};
 
 export default Missions;
