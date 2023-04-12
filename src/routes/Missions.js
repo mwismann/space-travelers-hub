c59260a6b518243d03d9dbe9572858532b983d45
getssh <gtamrat33@gmail.com>
2023-04-12 12:34:04 +0300

    Rendered mission name and descriptions

--- src/routes/Missions.js
+++ src/routes/Missions.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getMissons } from '../redux/missions/missionSlice';
 
 const Missions = () => {
-  const { missions } = useSelector((state) => state.missions);
+  const { missions, isLoading, error } = useSelector((state) => state.missions);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -11,7 +11,36 @@ const Missions = () => {
   }, [dispatch]);
 
   return (
-    <div>{JSON.stringify(missions)}</div>
+    <div>
+      {
+        isLoading
+        && <div>Loading...</div>
+      }
+      {
+        error
+        && <div>Error</div>
+      }
+      <div>
+        {
+          missions
+          && (
+          <div>
+            {' '}
+            {
+            missions.map((mission) => (
+              <div key={mission.mission_id}>
+                <p>{mission.mission_name}</p>
+                <p>{mission.description}</p>
+              </div>
+            ))
+          }
+            {' '}
+
+          </div>
+          )
+        }
+      </div>
+    </div>
   );
 };
 
