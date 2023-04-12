77b537bfd9c2fbf4f3c5bace7e310dac88003b5b
Matt <mwbertolucci@gmail.com>
2023-04-12 13:03:30 -0400

    Display Rockets

--- src/routes/Rockets.js
+++ src/routes/Rockets.js
@@ -1,5 +1,34 @@
-const Rockets = () => (
-  <div>Rockets</div>
-);
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import getRockets from '../redux/rockets/actions';
+import Rocket from '../components/Rocket';
+
+const Rockets = () => {
+  const { rockets, isLoading, error } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
+
+  return (
+    <div className="rockets-container">
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Upss! Something went wrong...</div>}
+      <ul>
+        {rockets.map((rocket) => (
+          <Rocket
+            key={rocket.id}
+            id={rocket.id}
+            name={rocket.rocket_name}
+            description={rocket.description}
+            img={rocket.flickr_images}
+            isReserved={rocket.isReserved}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Rockets;
