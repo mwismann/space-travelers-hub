d9c8d0e27d08579cf723f682bd554346379d19c9
Matt <mwbertolucci@gmail.com>
2023-04-12 13:02:55 -0400

    Add Rocket component

---
+++ src/components/Rocket.js
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRocket, cancelReservation } from '../redux/rockets/rocketsSlice';
+
+const Rocket = ({
+  id, name, description, img, isReserved,
+}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <li>
+      <div>
+        <img src={img} alt="Rocket" />
+      </div>
+      <div>
+        <h2>{name}</h2>
+        <div>
+          {isReserved && <span>Reserved</span>}
+          <p>{description}</p>
+        </div>
+        {(isReserved
+          ? (
+            <button
+              type="button"
+              onClick={() => dispatch(cancelReservation(id))}
+            >
+              Cancel Reservation
+            </button>
+          )
+          : (
+            <button
+              type="button"
+              onClick={() => dispatch(reserveRocket(id))}
+            >
+              Reserve Rocket
+            </button>
+          ))}
+      </div>
+    </li>
+  );
+};
+
+Rocket.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  isReserved: PropTypes.bool.isRequired,
+};
+
+export default Rocket;
