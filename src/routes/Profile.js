393dcc98d95ff189cc62e52c85726087ec18cf94
Matt <mwbertolucci@gmail.com>
2023-04-12 15:20:50 -0400

    Add Missions to profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -2,11 +2,20 @@ import { useSelector } from 'react-redux';
 
 const Profile = () => {
   const { rockets } = useSelector((state) => state.rockets);
+  const { missions } = useSelector((state) => state.missions);
 
   return (
     <div>
       <div>
-        <h2>Reserved Rockets</h2>
+        <h2>My Missions</h2>
+        <ul>
+          {missions.filter((mission) => mission.reserved === true).map((mission) => (
+            <li key={mission.mission_id}>{mission.mission_name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>My Rockets</h2>
         <ul>
           {rockets.filter((rocket) => rocket.isReserved === true).map((rocket) => (
             <li key={rocket.id}>{rocket.rocket_name}</li>
