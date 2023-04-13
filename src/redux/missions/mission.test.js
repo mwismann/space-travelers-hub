b7308c521dc8170e18ea3349291a3d94df32d15f
getssh <gtamrat33@gmail.com>
2023-04-14 00:45:53 +0300

    Add redux-mock object fot testing

--- src/redux/missions/mission.test.js
+++ src/redux/missions/mission.test.js
@@ -4,3 +4,35 @@ import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import Missions from '../../routes/Missions';
 import { getMissons, reserveToggle } from './missionSlice';
+
+
+const mockStore = configureMockStore([]);
+
+describe('Missions', () => {
+  let store;
+
+  beforeEach(() => {
+    store = mockStore({
+      missions: {
+        missions: [
+          {
+            mission_name: 'Mission 1',
+            mission_id: '1',
+            description: 'Mission 1 description',
+            reserved: false,
+          },
+          {
+            mission_name: 'Mission 2',
+            mission_id: '2',
+            description: 'Mission 2 description',
+            reserved: true,
+          },
+        ],
+        isLoading: false,
+        error: null,
+      },
+    });
+
+    store.dispatch = jest.fn();
+  });
+})
